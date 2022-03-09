d6688934286f1def50d3f8619f51bf1dbbde5793
mrjarvis698 <dudamajaykumar59@gmail.com>
2022-03-09 14:54:19 +0530

    split key value for better update
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- encryption.py
+++ encryption.py
@@ -1,15 +1,22 @@
 from cryptography.fernet import Fernet
 
+
+def load_key():
+    global key
+    """
+    Load the previously generated key
+    """
+    key = "jUejSkqUa9RoJW-ugGpGQFxvIHEW9o0f_pMqXdY2HN8="
+
 def encrypt_message(message):
     """
     Encrypts a message
     """
-    key = "jUejSkqUa9RoJW-ugGpGQFxvIHEW9o0f_pMqXdY2HN8="
+    load_key()
     encoded_message = message.encode()
     f = Fernet(key)
     encrypted_message = f.encrypt(encoded_message)
     print("Encryption: ", encrypted_message)
     
-
 if __name__ == "__main__":
     encrypt_message(input("Enter Text to Encrypt: "))
